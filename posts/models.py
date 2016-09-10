87df9d28e58549b27cad38f58a8694d7d429f124
Emiliano Montero rasgado <emilianomonterorasgado@MacBook-Pro-de-Emiliano.local>
2016-09-10 13:54:03 -0500

    estres

--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class Categoria(models.Model):
 	nombre = models.CharField(max_length=140)
-	#posts = models.ManyToManyField(Post, related_name='categorias')
 
 	def __str__(self):
 		return self.nombre
