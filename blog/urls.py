72509063e0a8c1364db94d9ed58ff38ca2a3d157
angel-blas <angeluzumaki1@outlook.com>
2016-09-16 18:19:12 -0500

    Se puede eliminar

--- blog/urls.py
+++ blog/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     url(r'^blog/categoria/(?P<cat>[-\w]+)/$', views.ListView.as_view(), name="categoria"),
     url(r'^blog/(?P<slug>[-\w]+)/$', views.DetailView.as_view(), name="detalle"),
     url(r'^accounts/',include(cuentasUrls)),
+    url(r'^editar/(?P<pk>\d+)/$', views.PostUpdate.as_view(), name="update"),
+    url(r'^borrar/(?P<pk>\d+)/$', views.PostDelete.as_view(), name="delete"),
     url(
     	regex=r'media/(?P<path>.*)$',
     	view=serve,
